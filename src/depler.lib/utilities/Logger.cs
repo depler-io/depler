26ff2791e2d768141fbbfa5c01ad4a34c9a6bd16
Todor Todorov <totollygeek@users.noreply.github.com>
2022-05-16 21:07:27 +0300

    Blah

---
+++ src/depler.lib/utilities/Logger.cs
@@ -0,0 +1,24 @@
+﻿using Serilog;
+
+namespace depler.lib.utilities;
+
+public static class Logger
+{
+    public static void Configure()
+    {
+        Log.Logger = new LoggerConfiguration()
+            .ConfigureConsole()
+            .ConfigureLevel()
+            .CreateLogger();
+    }
+    
+    private static LoggerConfiguration ConfigureConsole(this LoggerConfiguration configuration)
+    {
+        return configuration.WriteTo.Console();
+    }
+
+    private static LoggerConfiguration ConfigureLevel(this LoggerConfiguration configuration)
+    {
+        return configuration.MinimumLevel.Verbose();
+    }
+}
