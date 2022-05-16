8a42909b003bcc6c5f3855179ca824f7d2c1fc78
Todor Todorov <totollygeek@users.noreply.github.com>
2022-05-16 21:07:27 +0300

    Blah

--- src/depler.console/Program.cs
+++ src/depler.console/Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using depler.lib.utilities;
 
-Console.WriteLine("Hello, World!");
+Logger.Configure();
+
+if (args.Length < 1)
+{
+    Serilog.Log.Error(
+        "You must pass at least one command to [underline lime]depler[/]. Use [yellow]depler --help[/] to see available options");
+    return -1;
+}
+
+return 0;
---
+++ src/depler.lib/extensions/ConsoleUtilities.cs
@@ -0,0 +1,19 @@
+﻿using Spectre.Console;
+namespace depler.lib.extensions;
+
+public static class ConsoleUtilities
+{
+    public static void ToConsole(this string input, Color color = new(), bool escape = true, bool newLine = true)
+    {
+        if (newLine)
+        {
+            if (escape) AnsiConsole.MarkupLineInterpolated($"[{color}]{input}[/]");
+            else AnsiConsole.MarkupLine($"[{color}]{input}[/]");
+        }
+        else
+        {
+            if (escape) AnsiConsole.MarkupInterpolated($"[{color}]{input}[/]"); 
+            else AnsiConsole.Markup($"[{color}]{input}[/]");
+        }
+    }
+}
